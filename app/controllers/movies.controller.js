3ae553d97bd07355e87d4a7259ecaa3d0c25c399
Andrey Gorky <gorkaviya@gmail.com>
2021-10-04 21:15:18 +0300

    more refactoring

--- app/controllers/movies.controller.js
+++ app/controllers/movies.controller.js
@@ -1,5 +1,4 @@
 const Movie =  require('../models/movies.model');
-const { move } = require('../routes/index.route');
 
 exports.createOne = async (req, res, next) => {
     try {
@@ -16,25 +15,6 @@ exports.createOne = async (req, res, next) => {
     } catch (error) {
         return res.status(404).json(error);
     }
-
-    // try {
-    //     // return res.status(200).json({"message": "Movie CREATED"});
-    //     const MOVIE_MODEL = {
-    //         title: req.body.Title,
-    //         release: req.body.Release,
-    //         format: req.body.Format,
-    //     };
-
-    //     try {
-    //         const movie = await Movie.create(MOVIE_MODEL);
-    //         console.log("Movie Created");
-    //         return res.status(201).json(movie)
-    //     } catch (error) {
-    //         return res.status(500).json(error);
-    //     }
-    // } catch (error) {
-    //     return res.status(500).json(error);
-    // }
 }
 
 exports.deleteOne = (req, res, next) => {
@@ -42,14 +22,6 @@ exports.deleteOne = (req, res, next) => {
         .destroy({ where: { id: req.params.id } })
         .then(() => res.status(204).json({message: "Movie deleted"}))
         .catch(error => res.status(500).json(error));
-    
-    // try {
-    //     // return res.status(200).json({"message": "Movie DELETED"});
-    //     const movie = await Movie.destroy({ where: { id: req.params.id } });
-    //     return res.status(200).json(movie);
-    // } catch (error) {
-    //     return res.status(500).json(error);
-    // }
 }
 
 exports.findOne = (req, res, next) => {
@@ -59,14 +31,6 @@ exports.findOne = (req, res, next) => {
             if (movie) return res.status(200).send(movie);
             return res.status(404).json({message: "Movie not found"});
         });
-
-    // try {
-    //     // return res.status(200).json({"message": "SHOW Movie Info"});
-    //     const movie = await Movie.findByPk(req.params.id);
-    //     return res.status(200).json(movie);
-    // } catch (error) {
-    //     return res.status(500).json(error);
-    // }
 }
 
 exports.findAll = async (req, res, next) => {
@@ -76,14 +40,6 @@ exports.findAll = async (req, res, next) => {
             if (movies) return res.status(200).send(movies);
             return res.status(404).json({message: "Movies not found"});
         });    
-    
-    // try {
-    //     // return res.status(200).json({"message": "Movie List Sorted By Title in Alphabetic Order"});
-    //     const movies = await Movie.findAll({ order: [['title', 'ASC']] });
-    //     return res.status(200).json(movies);
-    // } catch (error) {
-    //     return res.status(500).json(error);
-    // }
 }
 
 exports.findOneByTitle = (req, res, next) => {
@@ -94,16 +50,6 @@ exports.findOneByTitle = (req, res, next) => {
             if (movie) return res.status(200).send(movie);
             return res.status(404).json({message: "Movie not found"});
         });
-    
-    
-    // try {
-    //     // return res.status(200).json({"message": "Movie Found By Title"});
-    //     const title = req.params.title
-    //     const movie = await Movie.findOne({ where: { title: title } });
-    //     return res.status(200).json(movie);
-    // } catch (error) {
-    //     return res.status(400).json(error);
-    // }
 }
 
 exports.findAllByActor = async (req, res, next) => {
